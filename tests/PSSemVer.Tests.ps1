17b8e946dc99f102b949d06f8ead9e19c926b6ba
Marius Storhaug <marstor@hotmail.com>
2024-11-02 17:15:36 +0100

    🩹 [Patch]: Remove redundant parameter from tests (#25)
    ## Description
    
    - Remove redundant parameter from tests
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/PSSemVer.Tests.ps1
+++ tests/PSSemVer.Tests.ps1
@@ -1,13 +1,4 @@
-﻿[CmdletBinding()]
-Param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
-
-Describe 'PSSemVer' {
+﻿Describe 'PSSemVer' {
     Describe 'Function: New-PSSemVer' {
         It "'New-PSSemVer -Major 1 -Minor 2 -Patch 3' => '1.2.3'" {
             $PSSemVer = New-PSSemVer -Major 1 -Minor 2 -Patch 3
